0e50d4fe02d8b489abe195f949b330f9fb06df82
Dragan Atanasov <d.atanasov@macopedia.pl>
2014-12-23 10:57:59 +0100

    fixed GTM fatal error in orders without shipping address

--- app/code/community/CVM/GoogleTagManager/Block/Gtm.php
+++ app/code/community/CVM/GoogleTagManager/Block/Gtm.php
@@ -89,7 +89,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'transactionTax' => round($order->getBaseTaxAmount(),2),
 					'transactionPaymentType' => $order->getPayment()->getMethodInstance()->getTitle(),
 					'transactionCurrency' => $order->getOrderCurrencyCode(),
-					'transactionShippingMethod' => $order->getShippingCarrier()->getCarrierCode(),
+					'transactionShippingMethod' => !$order->getIsVirtual() ? $order->getShippingCarrier()->getCarrierCode() : '',
 					'transactionPromoCode' => $order->getCouponCode(),
 					'transactionProducts' => array()
 				);
